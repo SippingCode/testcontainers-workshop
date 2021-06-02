77f84bd6e4cebd2ff34a4dd1344f17b681428f7b
Abdelillah Hassam <ahassam@itpps.com>
2021-06-02 11:14:48 +0200

    test containters workshop

---
+++ test-containers/src/main/java/com/testcontainers/workshop/PersonRepository.java
@@ -0,0 +1,6 @@
+package com.testcontainers.workshop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
---
+++ test-containers/src/test/java/com/testcontainers/workshop/ApplicationIT.java
@@ -0,0 +1,34 @@
+package com.testcontainers.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApplicationIT {
+
+  @Container
+  public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
+    .withPassword("inmemory")
+    .withUsername("inmemory");
+
+  @DynamicPropertySource
+  static void postgresqlProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+    registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+  }
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
